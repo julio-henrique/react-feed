719e86686ed3eaa2ee2bc6d4bbdfc4347ffa44af
Julio Henrique <juliohjesus@gmail.com>
2022-08-29 10:36:20 -0300

    starting feed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './global.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>Hello World</h2>
+      <h3>Standing here I realize...</h3>
+    </>
+  )
+}
+
+export default App
