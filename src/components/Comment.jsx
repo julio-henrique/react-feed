f588880a17342673a98e7fd8b41d8cce65377319
Julio Henrique <juliohjesus@gmail.com>
2022-09-14 22:18:04 -0300

    adding comments component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import { ThumbsUp, Trash } from 'phosphor-react'
+
+import { Avatar } from './Avatar'
+
+import styles from './Comment.module.jsx'
+
+interface CommentProps {
+    content: string
+    onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount(prev => prev + 1)
+    }
+
+    return (
+        <div className={styles.comment}>
+            <div className={styles.commentContent}>
+                <header>
+                    <div className={authorAndTime}>
+                        <strong>Júlio Henrique</strong>
+                        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+                    </div>
+
+                    <button onClick={handleDeleteComment} title='Deletar comentário'>
+                        <Trash size={24} />
+                    </button>
+                </header>
+
+                <p>{content}</p>
+            </div>
+
+            <footer>
+                <button onClick={handleClickComment}>
+                    <ThumbsUp />
+                    Aplaudir <span>{likeCount}</span>
+                </button>
+            </footer>
+        </div>
+    )
+}
