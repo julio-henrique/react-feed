7aee22a28d975bec57d6458d7c832fe98dc9ff7e
Julio Henrique <juliohjesus@gmail.com>
2022-09-15 22:49:28 -0300

    Comment bug correction

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,49 +1,53 @@
-import { useState } from 'react'
-import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from "react";
+import styles from './Comment.module.css';
 
-import { Avatar } from './Avatar'
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
-import styles from './Comment.module.jsx'
-
-interface CommentProps {
-    content: string
-    onDeleteComment: (comment: string) => void
-}
-
-export function Comment({ content, onDeleteComment }: CommentProps) {
-    const [likeCount, setLikeCount] = useState(0)
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
 
     function handleDeleteComment() {
-        onDeleteComment(content)
+        onDeleteComment(content);
     }
 
     function handleLikeComment() {
-        setLikeCount(prev => prev + 1)
+        setLikeCount((state) => {
+            return state + 1
+        });
     }
 
     return (
         <div className={styles.comment}>
-            <div className={styles.commentContent}>
-                <header>
-                    <div className={authorAndTime}>
-                        <strong>Júlio Henrique</strong>
-                        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
-                    </div>
+            <Avatar
+                hasBorder={false}
+                src="https://randomuser.me/api/portraits/men/60.jpg"
+                alt=""
+            />
 
-                    <button onClick={handleDeleteComment} title='Deletar comentário'>
-                        <Trash size={24} />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Diego Fernandes</strong>
+                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
                     </button>
-                </header>
-
-                <p>{content}</p>
+                </footer>
             </div>
-
-            <footer>
-                <button onClick={handleClickComment}>
-                    <ThumbsUp />
-                    Aplaudir <span>{likeCount}</span>
-                </button>
-            </footer>
         </div>
     )
 }
