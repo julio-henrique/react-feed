920b79f280b3c231b90d3b7a32747cc4d20929d4
Julio Henrique <juliohjesus@gmail.com>
2022-09-16 15:47:54 -0300

    Header bug correction

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import styles from './Header.module.css'
 import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
-    <header className={styles.header}>
-        <img src={igniteLogo} alt="Logo" />
-    </header>
+    return (
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Logo" />
+        </header>
+    )
 }
