6e2249a73631837d85d9698c9456a1fc9a041370
Julio Henrique <juliohjesus@gmail.com>
2022-09-15 22:46:40 -0300

    Avatar bug correction

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,11 +1,6 @@
-import { ImgHTMLAttributes } from "react";
 import styles from './Avatar.module.css'
 
-interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
-    hasBorder?: boolean;
-}
-
-export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
+export function Avatar({ hasBorder = true, ...props }) {
     return (
         <img
             className={hasBorder ? styles.avatarWithBorder : styles.avatar}
